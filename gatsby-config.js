2d4a111196343c6a8aa31c8a5860086f0e4cd65e
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-11-18 22:58:37 +0900

    post폴더 통일

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,13 @@ module.exports = {
           name: "markdown-pages"
         }
       },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: `${__dirname}/_posts`,
+          name: "markdown-pages"
+        }
+      },
     `gatsby-transformer-remark`
   ]
 };
