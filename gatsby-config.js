16f7df391af405e132533a79a2f42eaad4167d93
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-11-20 16:20:49 +0900

    tag fields 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,6 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-netlify-cms`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/blog`,
-        name: "markdown-pages"
-      }
-    },
-    {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          path: `${__dirname}/study`,
-          name: "markdown-pages"
-        }
-      },
       {
         resolve: `gatsby-source-filesystem`,
         options: {
