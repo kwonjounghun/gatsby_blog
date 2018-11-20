90ccec3dc1bed7fdf00758830bef9af9a7829519
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-11-20 15:54:16 +0900

    layout component 만들기

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
-import React from "react"
+import React from "react";
+import Layout from "../layout";
 
-export default () => <div>Hello world!</div>
+export default () => <Layout><div>Hello world!</div></Layout>
