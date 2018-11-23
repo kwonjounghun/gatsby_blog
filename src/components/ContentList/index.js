b9289c8fc7ed649359d10e5169b2cb201a19a076
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-11-23 17:31:14 +0900

    itemlist styling 진행중

--- src/components/ContentList/index.js
+++ src/components/ContentList/index.js
@@ -1,13 +1,32 @@
 import React from "react";
+import ContentItem from "../ContentItem";
+import styled, { css, ThemeProvider } from "styled-components";
+import PxtoRem from "../../modules/PxtoRem";
+import Themes from "../../styled-components/themes";
+
+const List = styled.ul(props => {
+  return css`
+    width: 100%;
+    ${props.theme.Media.xlarge`width: ${PxtoRem(800)};`}
+    ${props.theme.Media.large`width: ${PxtoRem(800)};`}
+  `;
+});
 
 class ContentList extends React.Component {
-    render() {
-        return (
-            <>
-                contentList
-            </>
-        )
-    }
+  state = {
+    items: [1, 2, 3, 4, 5]
+  };
+  render() {
+    return (
+      <ThemeProvider theme={Themes}>
+        <List className="clearFix">
+          {this.state.items.map((item, index) => {
+            return <ContentItem key={`ContentItem${index}`} />;
+          })}
+        </List>
+      </ThemeProvider>
+    );
+  }
 }
 
 export default ContentList;
