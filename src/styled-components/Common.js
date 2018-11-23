9cd3ee4bcc41aa4437366d88b44dea504edf8c6f
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-11-23 17:32:26 +0900

    xlarge size 수정

--- src/styled-components/Common.js
+++ src/styled-components/Common.js
@@ -4,9 +4,8 @@ import PxtoRem from "../modules/PxtoRem";
 export const Content = styled.div(props => {
     return (css`
         background-color: yellow;
-        height: 100vh;
         margin: 0 auto;
-        ${props.theme.Media.xlarge`width: ${PxtoRem(props.theme.BreakPoint.xlarge.min-100)};`}
+        ${props.theme.Media.xlarge`width: ${PxtoRem(props.theme.BreakPoint.large.min-100)};`}
         ${props.theme.Media.large`width: ${PxtoRem(props.theme.BreakPoint.large.min-100)};`}
         ${props.theme.Media.desktop`width: ${PxtoRem(props.theme.BreakPoint.desktop.min-100)};`}
         ${props.theme.Media.tablet`width: ${PxtoRem(props.theme.BreakPoint.tablet.min-100)};`}
