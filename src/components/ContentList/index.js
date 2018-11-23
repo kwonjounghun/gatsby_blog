35323be5a5109d3c57ee3f297b91abd72e011bab
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-11-23 14:03:42 +0900

    contentlist component 생성

---
+++ src/components/ContentList/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ContentList extends React.Component {
+    render() {
+        return (
+            <>
+                contentList
+            </>
+        )
+    }
+}
+
+export default ContentList;
