0630fb05fbc773e4dc7d7fc3fae3cd780e6db682
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-11-23 14:01:40 +0900

    반응형 content적용

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,3 +1,36 @@
-import React from "react"
+import React from "react";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import Header from "../components/Header";
+import Themes from "../styled-components/themes";
+import Helmet from "react-helmet";
+import { Content } from "../styled-components/Common";
 
-export default ({ children }) => <div className="app-wrapper">{children}</div>
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    color: ${props => (props.whiteColor ? "white" : "black")};
+    font-family: ${props => props.theme.Fonts.family};
+  }
+`;
+
+export default ({ children }) => {
+  return (
+    <>
+      <Helmet>
+        <title>블로그</title>
+        <link
+          href="https://fonts.googleapis.com/css?family=Noto+Sans+KR:100,300,400,500,700"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <ThemeProvider theme={Themes}>
+        <div className="app-wrapper">
+          <GlobalStyle />
+          <Header />
+          <Content>{children}</Content>
+        </div>
+      </ThemeProvider>
+    </>
+  );
+};
