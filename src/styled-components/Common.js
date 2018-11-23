688928ea7c272b0c33f2e7d006333d474a27ad4e
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-11-23 14:01:22 +0900

    반응형 content 사이즈 생성

---
+++ src/styled-components/Common.js
@@ -0,0 +1,15 @@
+import styled, {css} from "styled-components";
+import PxtoRem from "../modules/PxtoRem";
+
+export const Content = styled.div(props => {
+    return (css`
+        background-color: yellow;
+        height: 100vh;
+        margin: 0 auto;
+        ${props.theme.Media.xlarge`width: ${PxtoRem(props.theme.BreakPoint.xlarge.min-100)};`}
+        ${props.theme.Media.large`width: ${PxtoRem(props.theme.BreakPoint.large.min-100)};`}
+        ${props.theme.Media.desktop`width: ${PxtoRem(props.theme.BreakPoint.desktop.min-100)};`}
+        ${props.theme.Media.tablet`width: ${PxtoRem(props.theme.BreakPoint.tablet.min-100)};`}
+        ${props.theme.Media.phone`width: 100%;`}
+    `)
+})
