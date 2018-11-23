3e23f4023466f0b3ced2e3fdc48cca58ef944825
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-11-23 17:31:59 +0900

    list test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from "../layout";
+import ContentList from "../components/ContentList";
 
 class Pages extends React.Component {
     render(){
         return (
-            <Layout><div>Hello world!</div></Layout>
+            <Layout><ContentList/></Layout>
         )
     }
 }
