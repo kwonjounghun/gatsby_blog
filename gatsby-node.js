715eb2f7cd6db871cadfa83c7b62c112e4bca1f3
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-11-24 13:54:12 +0900

    list페이지 생성하도록 create함수 생성

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,36 +1,87 @@
-const path = require("path")
+const path = require("path");
 
-exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions;
 
-  const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`)
+  const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`);
+  const blogListPageTemplate = path.resolve(
+    `src/templates/blogListTemplate.js`
+  );
 
-  return graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            frontmatter {
-              path
+  const createListPage = async _ => {
+    return graphql(`
+      {
+        allMarkdownRemark(
+          sort: { order: DESC, fields: [frontmatter___date] }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              excerpt
+              frontmatter {
+                path,
+                category,
+                label,
+                title,
+                thumbnail,
+                date
+              }
             }
           }
         }
       }
-    }
-  `).then(result => {
-    if (result.errors) {
-      return Promise.reject(result.errors)
-    }
+    `).then(result => {
+      if (result.errors) {
+        return Promise.reject(result.errors);
+      }
 
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      createPage({
-        path: node.frontmatter.path,
-        component: blogPostTemplate,
-        context: {}, // additional data can be passed via context
-      })
-    })
-  })
-}
+      result.data.allMarkdownRemark.edges.forEach(({ node }, index) => {
+        let path = `/${index}`;
+        if(index === 0){
+          path = "/"
+        }
+        createPage({
+          path: path,
+          component: blogListPageTemplate,
+          context: {
+            postLimit: 2,
+            skip: index*2
+          } // additional data can be passed via context
+        });
+      });
+    });
+  };
+
+  const createPostPage = async _ => {
+    return graphql(`
+      {
+        allMarkdownRemark(
+          sort: { order: DESC, fields: [frontmatter___date] }
+        ) {
+          edges {
+            node {
+              frontmatter {
+                path
+              }
+            }
+          }
+        }
+      }
+    `).then(result => {
+      if (result.errors) {
+        return Promise.reject(result.errors);
+      }
+
+      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+          path: node.frontmatter.path,
+          component: blogPostTemplate,
+          context: {} // additional data can be passed via context
+        });
+      });
+    });
+  };
+
+  await createListPage();
+  await createPostPage();
+};
