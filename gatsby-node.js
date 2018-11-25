97292658ffc5608f505417189619ed077ebbe5dd
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-11-26 02:34:34 +0900

    pagination 및 nav category 추가

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,12 +8,70 @@ exports.createPages = async ({ actions, graphql }) => {
     `src/templates/blogListTemplate.js`
   );
 
-  const createListPage = async _ => {
+  const createListPageFn = async (NavMenu, result, category) => {
+    const postLimit = 10;
+    let current = 1;
+    const edge = result.data.allMarkdownRemark.edges;
+    const totalPost = edge.length;
+    const totalPage = Math.ceil(totalPost / postLimit);
+    for (let i = 0; i < totalPage; i++) {
+      let path = `/${i}`;
+      if (i === 0 && category == null) {
+        path = `/`;
+      } else if (category == null) {
+        path = `/${i + 1}`;
+      } else if (category) {
+        path = `/${category}/${i + 1}`;
+      }
+      createPage({
+        path: path,
+        component: blogListPageTemplate,
+        context: {
+          NavMenu,
+          postLimit: postLimit,
+          skip: i * postLimit,
+          totalPage,
+          current: current,
+          category
+        } // additional data can be passed via context
+      });
+      current++;
+    }
+  };
+
+  const createListPage = async NavMenu => {
+    return graphql(`
+      {
+        allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+          edges {
+            node {
+              excerpt
+              frontmatter {
+                path
+                category
+                label
+                title
+                thumbnail
+                date
+              }
+            }
+          }
+        }
+      }
+    `).then(async result => {
+      if (result.errors) {
+        return Promise.reject(result.errors);
+      }
+      createListPageFn(NavMenu, result);
+    });
+  };
+
+  const createCategoryListPage = async (NavMenu, category) => {
     return graphql(`
       {
         allMarkdownRemark(
           sort: { order: DESC, fields: [frontmatter___date] }
-          limit: 1000
+          filter: { frontmatter: { category : { eq : "${category}" }}}
         ) {
           edges {
             node {
@@ -30,34 +88,57 @@ exports.createPages = async ({ actions, graphql }) => {
           }
         }
       }
-    `).then(result => {
+    `).then(async result => {
       if (result.errors) {
         return Promise.reject(result.errors);
       }
-
-      result.data.allMarkdownRemark.edges.forEach(({ node }, index) => {
-        let path = `/${index}`;
-        if(index === 0){
-          path = "/"
-        }
-        createPage({
-          path: path,
-          component: blogListPageTemplate,
-          context: {
-            postLimit: 2,
-            skip: index*2
-          } // additional data can be passed via context
-        });
-      });
+      await createListPageFn(NavMenu, result, category);
     });
   };
 
-  const createPostPage = async _ => {
+  const createCategoryListPageFn = async (NavMenu, category) => {
+    return category.forEach(async item => {
+      createCategoryListPage(NavMenu, item);
+    });
+  };
+
+  const getCategory = async _ => {
     return graphql(`
       {
-        allMarkdownRemark(
-          sort: { order: DESC, fields: [frontmatter___date] }
-        ) {
+        allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+          edges {
+            node {
+              frontmatter {
+                category
+              }
+            }
+          }
+        }
+      }
+    `).then(async result => {
+      if (result.errors) {
+        return Promise.reject(result.errors);
+      }
+      return DuplicateInspection(
+        result.data.allMarkdownRemark.edges,
+        "category"
+      );
+    });
+  };
+
+  const DuplicateInspection = (array, target) => {
+    return array.reduce((acc, current) => {
+      if (acc.indexOf(current.node.frontmatter[target]) < 0) {
+        acc.push(current.node.frontmatter[target]);
+      }
+      return acc;
+    }, []);
+  };
+
+  const createPostPage = async NavMenu => {
+    return graphql(`
+      {
+        allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
           edges {
             node {
               frontmatter {
@@ -76,12 +157,75 @@ exports.createPages = async ({ actions, graphql }) => {
         createPage({
           path: node.frontmatter.path,
           component: blogPostTemplate,
-          context: {} // additional data can be passed via context
+          context: {
+            NavMenu
+          } // additional data can be passed via context
         });
       });
     });
   };
 
-  await createListPage();
-  await createPostPage();
+  const getNavMenu = async _ => {
+    return graphql(`
+      {
+        allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+          edges {
+            node {
+              frontmatter {
+                label
+                category
+              }
+            }
+          }
+        }
+      }
+    `).then(result => {
+      if (result.errors) {
+        return Promise.reject(result.errors);
+      }
+      return result.data.allMarkdownRemark.edges;
+    });
+  };
+  const createNavMenu = async array => {
+    let menu = [];
+    let labels = await DuplicateInspection(array, "label");
+    // array는 menu key는 category, label
+    let isKey = async (array, key) => {
+      let result = false;
+      if(array.length === 0) return result;
+      for(let [index, item] of array.entries()){
+        if(item.key === key) return result = {index};
+        await setTimeout(()=>{}, 100);
+      }
+      return result;
+    };
+
+    // array는 array, menu는 menu
+    let addMenuItem = async (array, menu, isKey) => {
+      for(let item of array){
+        let {label, category} = item.node.frontmatter;
+        let isIndex = await isKey(menu, label);
+        if(isIndex) {
+          // key가 이미 존재할 경우
+          let isCateIndex = await isKey(menu[isIndex.index].list, category);
+          if (isCateIndex) {
+            menu[isIndex.index].list[isCateIndex.index].count += 1;
+          } else {
+            menu[isIndex.index].list.push({key: category, count: 1});
+          }
+        } else {
+          menu.push({key: label, list: [{key: category, count: 1}]});
+        }
+      }
+    }
+
+    await addMenuItem(array, menu, isKey);
+    return menu;
+  };
+  let categorys = await getCategory();
+  let menus = await getNavMenu();
+  let NavMenu = await createNavMenu(menus);
+  await createListPage(NavMenu);
+  await createCategoryListPageFn(NavMenu, categorys);
+  await createPostPage(NavMenu);
 };
