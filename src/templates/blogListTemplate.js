6a164f4cbe3057bd78dedc737a0fa14971dba750
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-11-26 02:37:45 +0900

    template에서 content관리하도록 변경

--- src/templates/blogListTemplate.js
+++ src/templates/blogListTemplate.js
@@ -1,23 +1,51 @@
 import React from "react";
 import { graphql } from "gatsby";
+import styled, { ThemeProvider } from "styled-components";
 import ContentList from "../components/ContentList";
 import Layout from "../layout";
+import Pagenation from "../components/Pagenation";
+import PxtoRem from "../modules/PxtoRem";
+import themes from "../styled-components/themes";
+
+const Content = styled.div`
+  margin-top: ${PxtoRem(25)};
+  width: 100%;
+  ${props => props.theme.Media.xlarge`width: ${PxtoRem(800)};`}
+  ${props => props.theme.Media.large`width: ${PxtoRem(800)};`}
+`;
 
 export default function Template({
-  data // this prop will be injected by the GraphQL query below.
+  data, // this prop will be injected by the GraphQL query below.
+  pathContext
 }) {
-  console.log(data);
   const { allMarkdownRemark } = data; // data.markdownRemark holds our post data
   const { edges } = allMarkdownRemark;
+  const { current, postLimit, skip, totalPage, category, NavMenu } = pathContext;
   return (
-    <Layout>
-      <ContentList list={edges} />
+    <Layout site={data.site.siteMetadata} NavMenu={NavMenu}>
+      <ThemeProvider theme={themes}>
+        <Content>
+          <ContentList list={edges} />
+          <Pagenation
+            current={current}
+            postLimit={postLimit}
+            skip={skip}
+            totalPage={totalPage}
+            category={category}
+          />
+        </Content>
+      </ThemeProvider>
     </Layout>
   );
 }
 
 export const pageQuery = graphql`
   query($skip: Int!, $postLimit: Int!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
     allMarkdownRemark(
       sort: { fields: [frontmatter___date], order: DESC }
       limit: $postLimit
@@ -25,7 +53,7 @@ export const pageQuery = graphql`
     ) {
       edges {
         node {
-          excerpt
+          excerpt(pruneLength: 280)
           frontmatter {
             path
             category
