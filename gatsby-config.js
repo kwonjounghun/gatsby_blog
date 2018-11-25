3cf6fde3534a2a3e599a1f68a1d316f8a0507a32
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-11-26 02:34:02 +0900

    google icon font 추가, post 내용 미리보기텍스트 길이 조절을 위한 plugin 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  siteMetadata: {
+    title: "내 블로그"
+  },
   plugins: [
     `gatsby-plugin-netlify-cms`,
       {
@@ -8,7 +11,21 @@ module.exports = {
           name: "markdown-pages"
         }
       },
-    `gatsby-transformer-remark`,
+      {
+        resolve: `gatsby-transformer-remark`,
+        options: {
+          "excerpt_separator": `<!-- end -->`
+        }
+      },
+      {
+        resolve: 'gatsby-plugin-google-fonts',
+        options: {
+          fonts: [
+            'material icons',
+            'roboto:300,400,500,700',
+          ],
+        },
+      },
     "gatsby-plugin-styled-components",
     `gatsby-plugin-react-helmet`
   ]
