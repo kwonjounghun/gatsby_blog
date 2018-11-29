e3100a3bd6535e6ed9c9ea59f9a4eb97b17b8f52
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-11-30 00:44:52 +0900

    blogTemplate 생성

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,27 +1,56 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from "react";
+import { graphql } from "gatsby";
+import styled, { ThemeProvider } from "styled-components";
+import Layout from "../layout";
+import PxtoRem from "../modules/PxtoRem";
+import themes from "../styled-components/themes";
+import Helmet from "react-helmet";
+
+
+const Content = styled.div`
+  margin-top: ${PxtoRem(25)};
+  width: 100%;
+  ${props => props.theme.Media.xlarge`width: ${PxtoRem(800)};`}
+  ${props => props.theme.Media.large`width: ${PxtoRem(800)};`}
+`;
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
+  pathContext
 }) {
-  const { markdownRemark } = data // data.markdownRemark holds our post data
-  const { frontmatter, html } = markdownRemark
+  const { markdownRemark } = data; // data.markdownRemark holds our post data
+  const { frontmatter, html } = markdownRemark;
+  const { NavMenu } = pathContext;
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-    </div>
-  )
+    <Layout site={data.site.siteMetadata} NavMenu={NavMenu}>
+      <ThemeProvider theme={themes}>
+        <Content>
+          <Helmet>
+            <link src="/static/assets/style/markdown.css"/>
+          </Helmet>
+          <div className="blog-post-container">
+            <div className="blog-post">
+              <h1>{frontmatter.title}</h1>
+              <h2>{frontmatter.date}</h2>
+              <div
+                className="blog-post-content markdown-body"
+                dangerouslySetInnerHTML={{ __html: html }}
+              />
+            </div>
+          </div>
+        </Content>
+      </ThemeProvider>
+    </Layout>
+  );
 }
 
 export const pageQuery = graphql`
   query($path: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
       frontmatter {
@@ -31,4 +60,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
