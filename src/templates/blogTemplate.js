58e3b4b9b788110049a010a0b9d098216e6275c1
kwonjounghun <kjh0102321@gmail.com>
2018-12-05 23:25:51 +0900

    post conent styling

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -15,6 +15,57 @@ const Content = styled.div`
   ${props => props.theme.Media.large`width: ${PxtoRem(800)};`}
 `;
 
+const BlogPost = styled.div`
+  display: block;
+  width: 100%;
+  background-color: white;
+  box-shadow: 0px 2px 10px 0px rgba(0,0,0,0.1);
+`;
+
+const BlogInfo = styled.div`
+  width: 100%;
+  position: relative;
+`;
+
+const PostContent = styled.div`
+  width: 100%;
+  padding: ${PxtoRem(25)};
+`;
+
+const BlogTitle = styled.h1`
+  position: absolute;
+  top: 50px;
+  left: 50px;
+  color: white;
+`;
+
+const BlogDate = styled.h2`
+  position: absolute;
+  top: 80px;
+  left: 50px;
+  color: white;
+`;
+
+const Thumbnail = styled.div`
+${props => console.log(props)}
+  width: 100%;
+  padding-top: 60%;
+  box-sizing: border-box;
+  background-image: url(${props => `"${props.image}"`});
+  background-position: center;
+  background-size: cover;
+  position: relative;
+  &::before{
+    position: absolute;
+    top: 0;
+    left: 0;
+    content: "";
+    display: block;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,0.7);
+  }
+`;
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
   pathContext
@@ -22,23 +73,27 @@ export default function Template({
   const { markdownRemark } = data; // data.markdownRemark holds our post data
   const { frontmatter, html } = markdownRemark;
   const { NavMenu } = pathContext;
+  console.log(frontmatter);
   return (
     <Layout site={data.site.siteMetadata} NavMenu={NavMenu}>
       <ThemeProvider theme={themes}>
         <Content>
           <Helmet>
-            <link src={MarkDownStyle}/>
+            <link src={MarkDownStyle} />
           </Helmet>
-          <div className="blog-post-container">
+          <BlogPost className="blog-post-container">
             <div className="blog-post">
-              <h1>{frontmatter.title}</h1>
-              <h2>{frontmatter.date}</h2>
-              <div
+              <BlogInfo>
+              <Thumbnail image={frontmatter.thumbnail} />
+              <BlogTitle>{frontmatter.title}</BlogTitle>
+              <BlogDate>{frontmatter.date}</BlogDate>
+              </BlogInfo>
+              <PostContent
                 className="blog-post-content markdown-body"
                 dangerouslySetInnerHTML={{ __html: html }}
               />
             </div>
-          </div>
+          </BlogPost>
         </Content>
       </ThemeProvider>
     </Layout>
@@ -56,8 +111,11 @@ export const pageQuery = graphql`
       html
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
-        path
-        title
+        path,
+        category,
+        label,
+        title,
+        thumbnail,
       }
     }
   }
