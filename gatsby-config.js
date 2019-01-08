19e346c5e7660370871aa50f0d4c05fafbe77684
kwonjounghun <kjh0102321@gmail.com>
2019-01-09 00:57:04 +0900

    ga 설치

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,26 @@ module.exports = {
         },
       },
     "gatsby-plugin-styled-components",
-    `gatsby-plugin-react-helmet`
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-131939758-1",
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Enables Google Optimize using your container Id
+        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "zenlog.netlify.com",
+      },
+    }
   ]
 };
